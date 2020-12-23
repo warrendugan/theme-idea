6e16f549e9cf767c2964eeaf633950866f2d746a
Warren Dugan <warrendugan@GMAIL.COM>
2020-12-23 15:41:43 -0800

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // add test-utils direct import
+  moduleDirectories: ['node_modules', 'test'],
+  // add jest-dom and emotion's extra matchers
+  setupFilesAfterEnv: ['<rootDir>/test/jest.setup.js'],
+  snapshotSerializers: ['@emotion/jest/serializer'],
+  // coverage
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx,js,jsx}'],
+};
